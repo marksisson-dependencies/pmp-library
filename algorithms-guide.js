1fc4d63622846079b1f5f4db9d4bf92f72f61c85
mbotsch <mbotsch@users.noreply.github.com>
2023-08-23 16:13:53 +0000

    deploy: 41b6933fc9e282bf12baad76c3ef699d4b2d3abb

---
+++ algorithms-guide.js
@@ -0,0 +1,18 @@
+var algorithms_guide =
+[
+    [ "Decimation", "decimation.html", [
+      [ "Parameters", "decimation.html#autotoc_md38", null ],
+      [ "Selections", "decimation.html#autotoc_md39", null ]
+    ] ],
+    [ "Remeshing", "remeshing.html", [
+      [ "Algorithm Basics", "remeshing.html#autotoc_md69", null ],
+      [ "Uniform Remeshing", "remeshing.html#autotoc_md70", null ],
+      [ "Adaptive Remeshing", "remeshing.html#autotoc_md71", null ],
+      [ "Preserving Sharp Features", "remeshing.html#autotoc_md72", null ],
+      [ "Selections", "remeshing.html#autotoc_md73", null ],
+      [ "Troubleshooting", "remeshing.html#autotoc_md74", null ]
+    ] ],
+    [ "Subdivision", "subdivision.html", null ],
+    [ "Smoothing", "smoothing.html", null ],
+    [ "Hole filling", "hole_filling.html", null ]
+];
---
+++ index.js
@@ -0,0 +1,8 @@
+var index =
+[
+    [ "Get Started", "index.html#autotoc_md76", null ],
+    [ "Read the Docs", "index.html#autotoc_md77", null ],
+    [ "Contribute", "index.html#autotoc_md78", null ],
+    [ "Acknowledge", "index.html#autotoc_md79", null ],
+    [ "License", "index.html#autotoc_md80", null ]
+];
